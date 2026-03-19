6aacb0bb610c31eb8989b78c5b2a70b7f7cc9645
juhairulislam <juhairulislam2018@gmail.com>
2026-03-19 16:27:07 +0600

    toggling using useState()

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ function App() {
 
   const [count, setCount] = useState(0) ;
 
+  const [isOn, setIsOn] = useState(false)
+
   const countHandler= () =>{
 
     setCount(count + 1) ;
@@ -14,7 +16,7 @@ function App() {
   return (
     <>
 <div className='text-center'>
-  <h1 className='text-5xl w-fit bg-gradient-to-r from-blue-500 to-pink-500 bg-clip-text text-transparent'>Hello World</h1>
+  <h1 className='text-5xl w-fit bg-gradient-to-r from-cyan-500  to-blue-500 bg-clip-text text-transparent mx-auto'>Understand useState()</h1>
 <br />
 <button className='my-4 text-4xl'>{count}</button>
 <br />
@@ -28,6 +30,20 @@ function App() {
 <br />
 
 <button className='bg-purple-500 p-2 rounded-xl ' onClick={()=>setCount(0)}>Reset</button>
+</div>
+
+
+<div className='text-center'>
+
+  <button className='bg-blue-500 p-2 rounded-md  my-5' onClick={()=>setIsOn(!isOn)}>
+    {isOn?'Turn On':'Turn Off'}
+    
+
+  </button>
+ <button className={`p-2 rounded-xl ml-4 ${isOn?'text-red-500 border border-red-400':'text-green-500 border border-green-500'}`}>
+  {isOn?'Off':'On'}
+ </button>
+
 </div>
 
  
