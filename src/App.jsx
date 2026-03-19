cd29c4319f9d3c63b8452daf0ce746819755b146
juhairulislam <juhairulislam2018@gmail.com>
2026-03-19 16:10:44 +0600

    understand useState of numbers

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,36 @@
 
+import { useState } from 'react'
 import './App.css'
 
 function App() {
 
+  const [count, setCount] = useState(0) ;
+
+  const countHandler= () =>{
+
+    setCount(count + 1) ;
+  }
+
   return (
     <>
-<h1 className='text-5xl w-fit bg-gradient-to-r from-blue-500 to-pink-500 bg-clip-text text-transparent'>Hello World</h1>
+<div className='text-center'>
+  <h1 className='text-5xl w-fit bg-gradient-to-r from-blue-500 to-pink-500 bg-clip-text text-transparent'>Hello World</h1>
+<br />
+<button className='my-4 text-4xl'>{count}</button>
+<br />
 
-    <div className="navbar bg-base-100 shadow-sm">
-  <div className="flex-1">
-    <a className="btn btn-ghost text-xl">daisyUI</a>
-  </div>
-  <div className="flex gap-2">
-    <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
-    <div className="dropdown dropdown-end">
-      <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-        <div className="w-10 rounded-full">
-          <img
-            alt="Tailwind CSS Navbar component"
-            src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
-        </div>
-      </div>
-      <ul
-        tabIndex="-1"
-        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-        <li>
-          <a className="justify-between">
-            Profile
-            <span className="badge">New</span>
-          </a>
-        </li>
-        <li><a>Settings</a></li>
-        <li><a>Logout</a></li>
-      </ul>
-    </div>
-  </div>
+<button className='bg-green-500 rounded-xl p-2 ' onClick={()=>countHandler(count)}>Increase</button>
+
+<br />
+
+<button onClick={()=>setCount(count - 1)} className='bg-red-500 p-2 rounded-xl my-3 '>Discrease</button>
+
+<br />
+
+<button className='bg-purple-500 p-2 rounded-xl ' onClick={()=>setCount(0)}>Reset</button>
 </div>
+
+ 
 
     </>
   )
